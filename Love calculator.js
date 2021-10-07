d4a6301278d3aa4bfc7f9239c210481866451dd9
Gurdarshan Singh <79242952+Gurdarshan-Singh@users.noreply.github.com>
2021-10-07 14:29:28 +0530

    Update Love calculator.js

--- Love calculator.js
+++ Love calculator.js
@@ -1,5 +1,10 @@
 prompt("Enter 1st person name ");
 prompt("Enter 2nd person name ");
 var n = Math.floor((Math.random()*100)+1);
+if(n>70){
+alert("Love Percentage is "+n+"% and You are are truely in love with each other");  
+}
+else{
 alert("Love Percentage is "+n+"%");
+}
 console.log(n);
