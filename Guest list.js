d5cf297eec4aea70663f1b07553a4fff358bfe4c
Gurdarshan Singh <79242952+Gurdarshan-Singh@users.noreply.github.com>
2021-10-07 17:38:48 +0530

    Create Guest list.js

---
+++ Guest list.js
@@ -0,0 +1,8 @@
+var guests = ["Jack","Jones","Jason","Laura"]
+var guest = prompt("What's your name?");
+if(guests.includes(guest)){
+  alert("Welcome");
+}
+else{
+  alert("Sorry , maybe next time");
+}
